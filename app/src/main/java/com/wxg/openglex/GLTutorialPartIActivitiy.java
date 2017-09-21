c2828a242f22070a96bfb80ddb2bc890a798b26f
wangxuguo <wangxuguo@szy.cn>
2017-09-21 15:18:49 +0800

    'init'

---
+++ app/src/main/java/com/wxg/openglex/GLTutorialPartIActivitiy.java
@@ -0,0 +1,26 @@
+package com.wxg.openglex;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * des
+ * author wangxuguo on 2017-09-21
+ * email  wangxuguo@szy.cn
+ * mark
+ */
+class GLTutorialPartIActivitiy extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new OpenGLRenderer());
+        setContentView(view);
+    }
+}
---
+++ app/src/main/java/com/wxg/openglex/OpenGLRenderer.java
@@ -0,0 +1,29 @@
+package com.wxg.openglex;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * des
+ * author wangxuguo on 2017-09-21
+ * email  wangxuguo@szy.cn
+ * mark
+ */
+class OpenGLRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
